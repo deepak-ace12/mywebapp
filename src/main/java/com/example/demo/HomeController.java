accf688edd111e6aef9c22b16f2066a257ba2104
Deepak Sharma <deepak15.sharma@paytminsurance.co.in>
2021-07-25 11:54:20 +0530

    added ModelAndView and removed sessions

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,20 +1,18 @@
 package com.example.demo;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("home")
-	public String home(HttpServletRequest req, HttpServletResponse res) { //The parameter HttpServletRequest, HttpServletResponse are not compulsory, but if you put it, spring will connect you to the request object
-		String name = req.getParameter("name");
-		HttpSession session = req.getSession();
-		session.setAttribute("name", name );
-		return "home";
+	public ModelAndView home(@RequestParam("name") String myName) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name", myName);
+		mv.setViewName("home");
+		return mv;
 	}
 }
